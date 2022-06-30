7f6915c7879743b4ebd9671752024a3d51695fb9
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-06-29 22:35:26 -0300

    Implementado o teste para verificar o nome da personagem

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,11 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  it('Verifica se o nome da personagem é Wonder Woman', async () => {
+    const character = await fetchCharacter('720');
+    // console.log(character); // Retorna o objeto
+    // console.log(character.name); // Retorna a porpriedade 'name' do objeto
+    expect(character.name).toBe('Wonder Woman');
+    // expect(character.name).toBe('Spider Man'); // Verifica se o teste não é um falso positivo. É esperado que falhe
+  });
 });
